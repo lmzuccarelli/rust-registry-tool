442ae9250a4ebdd0ab2e704ee2cb092083770bc5
Rafael Fonseca <r4f4rfs@gmail.com>
2025-02-15 22:56:21 +0100

    Some code cleanup
    Simplify some expressions and add return type to `main` so we can use
    the `?` operand shortcut.

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,20 @@ use crate::api::schema::*;
 use clap::Parser;
 use custom_logger as log;
 use mirror_auth::{get_token, ImplTokenInterface};
+use mirror_error::MirrorError;
 use mirror_query::*;
 use regex::Regex;
-use std::{fs, process};
+use std::{fs, str::FromStr};
 use tokio;
 
 mod api;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), MirrorError> {
     let args = Cli::parse();
-    let level = args.loglevel.unwrap().to_string();
-
-    // convert to enum
-    let res_log_level = match level.as_str() {
-        "info" => log::LevelFilter::Info,
-        "debug" => log::LevelFilter::Debug,
-        "trace" => log::LevelFilter::Trace,
-        _ => log::LevelFilter::Info,
-    };
+    let level = args.loglevel.as_deref().unwrap_or("info");
+    let res_log_level = log::LevelFilter::from_str(level)
+        .map_err(|_| MirrorError::new(&format!("invalid log level \"{level}\"")))?;
 
     // setup logging
     log::Logging::new()
@@ -29,31 +24,25 @@ async fn main() {
         .expect("should initialize");
     fs::create_dir_all("results").expect("should create results directory");
 
-    match &args.command {
+    match args.command {
         Some(Commands::ListCatalog {
             registry,
             namespace,
             no_tls_verify,
         }) => {
+            let url = format!(
+                "http{}://{registry}/v2/_catalog",
+                if no_tls_verify { "" } else { "s" },
+            );
+            log::trace!("url={url}");
             let t_impl = ImplTokenInterface {};
-            let token = get_token(
-                t_impl,
-                registry.to_string(),
-                namespace.to_string(),
-                !no_tls_verify,
-            )
-            .await;
+            let token = get_token(t_impl, registry, namespace, !no_tls_verify).await?;
             let i_query = ImplQueryImageInterface {};
-            let mut url = format!("https://{}/v2/_catalog", registry);
-            if *no_tls_verify {
-                url = url.replace("https", "http");
-            }
-            let res = i_query.get_details(url, token.unwrap(), false).await;
-            let rd = res.unwrap();
-            let res_json = serde_json::from_str(&rd.data);
-            let root: Catalogs = res_json.unwrap();
+            let rd = i_query.get_details(url, token, false).await?;
+            let root: Catalogs = serde_json::from_str(&rd.data)
+                .map_err(|e| MirrorError::new(&format!("should unmarshall response data: {e}")))?;
             for image in root.repositories.iter() {
-                log::info!("{}", image);
+                log::info!("{image}");
             }
         }
         Some(Commands::ListTags {
@@ -64,85 +53,62 @@ async fn main() {
             no_tls_verify,
             persist,
         }) => {
-            let re = Regex::new(r"4\.[0-9]{2}\.0").unwrap();
-            if !re.is_match(version) {
-                log::error!("format must respect the pattern -> '4.XX.0' where XX > 10");
-                process::exit(1);
+            let re = Regex::new(r"4\.[0-9]{2}\.0").expect("regex must compile");
+            if !re.is_match(&version) {
+                return Err(MirrorError::new(
+                    "format must respect the pattern -> '4.XX.0' where XX > 10",
+                ));
             }
             let cleaned_version = match name.as_str() {
-                "ocp-release" => {
-                    format!("{}", &version[..4])
-                }
+                "ocp-release" => version[..4].to_string(),
                 _ => format!("v{}", &version[..4]),
             };
             let t_impl = ImplTokenInterface {};
             let token = get_token(
                 t_impl,
-                registry.to_string(),
+                registry.clone(),
                 format!("{}/{}", namespace, name),
                 !no_tls_verify,
             )
-            .await;
+            .await?;
             let i_query = ImplQueryImageInterface {};
             let mut url = format!(
-                "https://{}/v2/{}/{}/tags/list?n=200&last={}",
-                registry.clone(),
-                namespace.clone(),
-                name.clone(),
-                cleaned_version.clone(),
+                "http{}://{registry}/v2/{namespace}/{name}/tags/list?n=200&last={cleaned_version}",
+                if no_tls_verify { "" } else { "s" },
             );
-            log::trace!("url {}", url);
-            if token.is_err() {
-                log::error!(
-                    "token {}",
-                    token.as_ref().err().unwrap().to_string().to_lowercase()
-                );
-                process::exit(1);
-            }
+            log::trace!("url {url}");
             let mut vec_tags: Vec<Tags> = Vec::new();
             let mut query = cleaned_version.to_string();
             let mut query_dump = "".to_string();
-            log::info!("querying {} ", registry.clone());
+            log::info!("querying {registry}");
 
-            while query.len() > 0 && query.contains(&cleaned_version) {
-                let res = i_query
-                    .get_details(url.clone(), token.as_ref().unwrap().to_string(), false)
-                    .await;
-                if res.is_err() {
-                    log::error!(
-                        "response {}",
-                        res.as_ref().err().unwrap().to_string().to_lowercase()
-                    );
-                    process::exit(1);
-                }
-                let rd = res.unwrap();
+            while !query.is_empty() && query.contains(&cleaned_version) {
+                let rd = i_query.get_details(url, token.clone(), false).await?;
                 query = rd.link;
-                log::trace!("query link {}", query.clone());
+                log::trace!("query link {query}");
                 if name == "ocp-release" && !query.contains(&cleaned_version) {
                     break;
                 }
-                url = format!("https://{}{}", registry.clone(), query);
-                query_dump.push_str(&format!("{}\n", query));
-                let res_json = serde_json::from_str(&rd.data);
-                let root: Tags = res_json.unwrap();
-                vec_tags.insert(0, root.clone());
+                url = format!(
+                    "http{}://{registry}{query}",
+                    if no_tls_verify { "" } else { "s" }
+                );
+                query_dump.push_str(&format!("{query}\n"));
+                let root: Tags = serde_json::from_str(&rd.data).map_err(|e| {
+                    MirrorError::new(&format!("could not parse response data: {e}"))
+                })?;
+                vec_tags.insert(0, root);
             }
 
-            let json_res = serde_json::to_string_pretty(&vec_tags);
-            if json_res.is_err() {
-                log::error!(
-                    "parsing json {}",
-                    json_res.as_ref().err().unwrap().to_string().to_lowercase()
-                );
-            }
-            if *persist {
-                fs::write(format!("results/{}.json", name.clone()), json_res.unwrap())
+            let json = serde_json::to_string_pretty(&vec_tags)
+                .map_err(|e| MirrorError::new(&format!("failed to marshal json: {e}")))?;
+            if persist {
+                fs::write(format!("results/{name}.json"), json)
                     .expect("should write json formatted results");
-                fs::write("results/links.txt".to_string(), query_dump)
-                    .expect("should write links list");
-                log::info!("file results/{}.json created successfully", name.clone());
+                fs::write("results/links.txt", query_dump).expect("should write links list");
+                log::info!("file results/{name}.json created successfully");
             } else {
-                log::info!("results {}", json_res.unwrap());
+                log::info!("results {json}");
             }
         }
         Some(Commands::ListTagsByUrl {
@@ -150,36 +116,23 @@ async fn main() {
             url,
             no_tls_verify,
         }) => {
-            let t_impl = ImplTokenInterface {};
-            let img_ref: Vec<&str> = url.split("/").collect::<Vec<&str>>();
+            let img_ref: Vec<&str> = url.split('/').collect();
             log::debug!("{:?}", img_ref);
+            let get_url = format!(
+                "http{}://{registry}{url}",
+                if no_tls_verify { "" } else { "s" }
+            );
+            let t_impl = ImplTokenInterface {};
             let token = get_token(
                 t_impl,
-                registry.to_string(),
-                format!("{}/{}", img_ref[2].to_string(), img_ref[3].to_string()),
+                registry,
+                format!("{}/{}", img_ref[2], img_ref[3]),
                 !no_tls_verify,
             )
-            .await;
+            .await?;
             let i_query = ImplQueryImageInterface {};
-            if token.is_err() {
-                log::error!(
-                    "token {}",
-                    token.as_ref().err().unwrap().to_string().to_lowercase()
-                );
-                process::exit(1);
-            }
-            let get_url = format!("https://{}{}", registry, url);
-            let res = i_query
-                .get_details(get_url, token.as_ref().unwrap().to_string(), false)
-                .await;
-            if res.is_err() {
-                log::error!(
-                    "response {}",
-                    res.as_ref().err().unwrap().to_string().to_lowercase()
-                );
-                process::exit(1);
-            }
-            log::info!("{}", res.unwrap().data);
+            let res = i_query.get_details(get_url, token, false).await?;
+            log::info!("{}", res.data);
         }
         Some(Commands::Digest {
             registry,
@@ -188,27 +141,20 @@ async fn main() {
             tag,
             no_tls_verify,
         }) => {
+            let url = format!(
+                "http{}://{registry}/v2/{namespace}/{name}/manifests/{tag}",
+                if no_tls_verify { "" } else { "s" },
+            );
             let t_impl = ImplTokenInterface {};
             let token = get_token(
                 t_impl,
-                registry.to_string(),
+                registry,
                 format!("{}/{}", namespace, name),
                 !no_tls_verify,
             )
-            .await;
+            .await?;
             let i_query = ImplQueryImageInterface {};
-            let mut url = format!(
-                "https://{}/v2/{}/{}/manifests/{}",
-                registry,
-                namespace.clone(),
-                name.clone(),
-                tag.clone(),
-            );
-            if *no_tls_verify {
-                url = url.replace("https", "http");
-            }
-            let res = i_query.get_details(url, token.unwrap(), true).await;
-            let rd = res.unwrap();
+            let rd = i_query.get_details(url, token, true).await?;
             log::info!("etag digest {}", rd.data);
         }
         Some(Commands::Copy {
@@ -216,13 +162,10 @@ async fn main() {
             to,
             no_tls_verify,
         }) => {
-            log::info!("{} {} {}", from, to, no_tls_verify);
+            log::info!("{from} {to} {no_tls_verify}");
             todo!()
         }
-
-        None => {
-            log::error!("sub command not recognized");
-            process::exit(1);
-        }
+        None => return Err(MirrorError::new("sub command not recognized")),
     }
+    Ok(())
 }
